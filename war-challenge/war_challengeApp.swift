871c28a22a69e081bbd1f9ca72f4928e872f00c2
shadvonic <moxeymarcus28@gmail.com>
2023-02-04 14:03:43 -0500

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Marc Moxey on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
